0e186fd7c9b6405409338ad948e41e9daaa1228a
Rasgris <kemuel.cruz@gmail.com>
2016-09-30 08:38:08 -0400

    Add files via upload

--- CanvasMini.js
+++ CanvasMini.js
@@ -10,7 +10,7 @@
 			  $("body").append('<div id="editorBox" style="background: rgba(0, 0, 0, 0.5); left: 0px; top: 0px; width: 100%; height: 100%; position: fixed; z-index: 10000;"><input id="openBox" style="display:none;margin: 0px auto; position: relative;" type="button" value="open"><div style="background: rgba(244, 244, 244, 1); padding: 5px; width: 80%; height: 90.5%; margin-left: 10%; box-shadow: 0px 0px 5px 2px black;"><style>#editor {padding: 20px; margin: 20px; width: 90%; height: 80%; }label{margin-right: 5px;}'+ 
 
 							   '<\/style>'+
-							   '<div style="padding: 5px; font-family: tahoma; margin-left: 2px;"><label>Mode: <\/label><select id="mode" size="1"><option value="ace/mode/javascript" selected>javascript<\/option><option value="ace/mode/html" selected>HTML<\/option><option value="ace/mode/css">CSS<\/option><option value="ace/mode/css">sql<\/option><\/select><label> Target: <\/label><select id="targetTA" size="1"><option><\/option>'+options+'<\/select><input type="button" id="submitTA" value="Close"><\/input><\/div>'+
+							   '<div style="padding: 5px; font-family: tahoma; margin-left: 2px;"><label>Mode: <\/label><select id="mode" size="1"><option value="ace/mode/javascript" selected>javascript<\/option><option value="ace/mode/html" selected>HTML<\/option><option value="ace/mode/css">CSS<\/option><option value="ace/mode/sql">sql<\/option><\/select><label> Target: <\/label><select id="targetTA" size="1"><option><\/option>'+options+'<\/select><input type="button" id="submitTA" value="Close"><\/input><\/div>'+
 							   '<div id="editor"><\/div>'+
 			
 			'<script>'+
